6ab2a4e777c32d23ecf0a8cab4d6f8b0aea7382d
sirpuffin1 <angelcruz981@yahoo.com>
2022-10-08 15:03:34 -0700

    feat(mid): utilize auth middleware

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const middleware = require('./middleware');
+const path = require('path')
 
 const server = app.listen(port, () => console.log("Server listening on port " + port))
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")))
+
+// Routes 
+const loginRoute = require('./routes/loginRoutes');
+
+app.use("/login", loginRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+    var payload = {
+        pageTitle: req.url
+    }
+    res.status(200).render('home', payload)
+})
