227676de8022157e3f74fd7b902c3960e48e355d
sirpuffin1 <angelcruz981@yahoo.com>
2022-10-09 12:05:17 -0700

    feat(register): create post route

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -9,4 +9,7 @@ router.get("/", (req, res, next) => {
     res.status(200).render('register')
 })
 
+router.post("/", (req, res, next) => {
+    res.status(200).render('register')
+})
 module.exports = router;
