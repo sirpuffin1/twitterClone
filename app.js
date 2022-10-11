aae309716152f9f91bb011cc686f7c0e7284f01e
sirpuffin1 <angelcruz981@yahoo.com>
2022-10-11 14:15:58 -0700

    chore(session): set up and utilize

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const middleware = require('./middleware');
 const path = require('path')
 const bodyParser = require('body-parser');
 const mongoose = require('./database');
+const session = require('express-session');
 
 
 const server = app.listen(port, () => console.log("Server listening on port " + port))
@@ -15,6 +16,11 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, "public")))
+app.use(session({
+    secret: "moose",
+    resave: true,
+    saveUninitialized: false,
+}))
 
 // Routes 
 const loginRoute = require('./routes/loginRoutes');
