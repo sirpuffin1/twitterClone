a2e3699aa65579ae170cba4463fecc58bd82fe2a
sirpuffin1 <angelcruz981@yahoo.com>
2022-10-11 15:05:23 -0700

    feat(db): set up db through mongoose

---
+++ database.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv').config();
+
+class Database {
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    mongoose
+      .connect(
+        process.env.MONGO_URL
+      )
+      .then(() => {
+        console.log("Database connection successful");
+      })
+      .catch((err) => {
+        console.log("Database connection error " + err);
+      });
+  }
+}
+
+module.exports = new Database();
