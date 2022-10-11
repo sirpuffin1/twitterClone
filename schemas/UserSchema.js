165416b46811605b395ab007eac0c35058773f0b
sirpuffin1 <angelcruz981@yahoo.com>
2022-10-11 13:53:50 -0700

    feat(user): update schema to set timestammps

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -2,38 +2,41 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "/images/profilePic.png",
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePic: {
-    type: String,
-    default: "/images/profilePic.png",
-  },
-});
+  { timestamps: true }
+);
 
-var User = mongoose.model('User', UserSchema)
+var User = mongoose.model("User", UserSchema);
 module.exports = User;
