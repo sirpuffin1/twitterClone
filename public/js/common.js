ba6f35bba31453540a1aca478a36fa9bc43586ba
sirpuffin1 <angelcruz981@yahoo.com>
2022-10-31 12:05:35 -0700

    feat(post): conditionally disable post button

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,16 @@
 $("#postTextarea").keyup(event => {
     var textbox = $(event.target);
     var value = textbox.val().trim()
-    console.log(value);
+    
+    var submitButton = $("#submitPostButton");
+
+    if(submitButton.length == 0) return alert("no submit button found")
+
+    if(value == "") {
+        submitButton.prop("disabled", true);
+        return;
+    }
+
+    submitButton.prop("disabled", false);
 })
+
