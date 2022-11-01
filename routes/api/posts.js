c8a9807c79ed6d90de209bcabcc116ec2c7eb030
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-01 15:41:47 -0700

    feat(api): create and setup api posts route

---
+++ routes/api/posts.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const User = require('../schemas/UserSchema');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+router.get("/", (req, res, next) => {
+})
+
+router.post("/", async (req, res, next) => {    
+})
+
+module.exports = router;
