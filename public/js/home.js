ddeed84a45592035c3e36bb42a849d0dc2209a42
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-05 10:30:40 -0700

    feat(home): receive and display posts

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,18 @@
 $(() => {
     $.get("/api/posts", posts => {
-        console.log(posts);
+        displayPosts(posts, $(".postsContainer"))
     })
 })
+
+function displayPosts(results, container) {
+    container.html("");
+
+    results.forEach(result => {
+        var html = createPostHtml(result);
+        container.append(html);
+    })
+
+    if(results.length == 0) {
+        container.append("<h2>Nothing to show</h2>")
+    }
+}
