1017b35b02c3cf0eb2fc21f770d97f5657cecb8a
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-05 11:06:32 -0700

    feat(posts): populate user information

--- public/js/common.js
+++ public/js/common.js
@@ -34,6 +34,10 @@ $("#submitPostButton").click(() => {
 function createPostHtml(postData) {
 
     var postedBy = postData.postedBy;
+
+    if(postedBy._id === undefined) {
+        return console.log("User object not populated");
+    }
     var displayName = postedBy.firstName + " " + postedBy.lastName;
     var timestamp = postData.createdAt;
 
