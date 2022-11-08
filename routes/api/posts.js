f9844ad134b4b363657a4fbf40caf1e838f7a7a4
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-08 13:16:58 -0800

    feat(api): add and populate replyto field

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -93,6 +93,10 @@ router.post("/", async (req, res, next) => {
         postedBy: req.session.user
     }
 
+    if(req.body.replyTo) {
+        postData.replyTo = req.body.replyTo;
+    }
+
     Post.create(postData)
     .then(async newPost => {
         newPost = await User.populate(newPost, { path: "postedBy"})
@@ -109,9 +113,11 @@ async function getPosts(filter) {
 var results = await Post.find(filter)
     .populate("postedBy")
     .populate("retweetData")
+    .populate("replyTo")
     .sort({"createdAt": -1})
     .catch(error => console.log(error))
 
+    results = await User.populate(results, {path: "replyTo.postedBy"})
     return await User.populate(results, {path: "retweetData.postedBy"})
 }
 
