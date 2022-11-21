2324c4799e2dca7b81d3b16ffddeb6d837cb76a3
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-20 20:47:48 -0800

    feat(user): update schema to hold followers/ing

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -35,7 +35,9 @@ const UserSchema = new Schema(
       default: "/images/profilePic.png",
     },
     likes: [{ type: Schema.Types.ObjectId, ref: "Post" }],
-    retweets: [{ type: Schema.Types.ObjectId, ref: "Post" }]
+    retweets: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    followers: [{ type: Schema.Types.ObjectId, ref: "User" }]
   },
   { timestamps: true }
 );
