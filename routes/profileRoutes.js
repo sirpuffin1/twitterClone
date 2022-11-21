15a8b04cf6ac2f401b2c7dcbe55f8bfc653ddd70
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-21 13:21:06 -0800

    feat(follow): create follow endpoints

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -28,6 +28,20 @@ router.get("/:username/replies", async(req, res, next) => {
     res.status(200).render('profilePage', payload)
 })
 
+router.get("/:username/following", async(req, res, next) => {
+    var payload = await getPayload(req.params.username, req.session.user);
+    // pass the "replies" selected tab along with other payload info like userLoggedIn
+    payload.selectedTab = "following";
+    res.status(200).render('followersAndFollowing', payload)
+})
+
+router.get("/:username/followers", async(req, res, next) => {
+    var payload = await getPayload(req.params.username, req.session.user);
+    // pass the "replies" selected tab along with other payload info like userLoggedIn
+    payload.selectedTab = "followers";
+    res.status(200).render('followersAndFollowing', payload)
+})
+
 async function  getPayload(username, userLoggedIn) {
     var user = await User.findOne({username: username});
 
