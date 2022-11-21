0096080c5078d48b9c62fa9185cddd6ac3148550
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-20 22:09:21 -0800

    ref(users): fix route

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,16 +19,16 @@ router.put("/:userId/follow", async(req, res, next) => {
      var isFollowing = user.followers && user.followers.includes(req.session.user._id);
      var option = isFollowing ? "$pull" : "$addToSet";
 
-     req.session.user = await User.findByIdAndUpdate(req.session.user._id, { [option]: {following: userId}}, { new: true})
+     req.session.user = await User.findByIdAndUpdate(req.session.user._id, { [option]: { following: userId } }, { new: true})
      .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
+         console.log(error);
+         res.sendStatus(400);
      })
 
-     User.findByIdAndUpdate(userId, { [option]: {followers: userId } })
+     User.findByIdAndUpdate(userId, { [option]: { followers: req.session.user._id } })
      .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
+         console.log(error);
+         res.sendStatus(400);
      })
 
      res.status(200).send(req.session.user);
