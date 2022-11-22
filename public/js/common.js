8a6d3e847cacef4e2558b64ff90b9726b4f8c703
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-22 10:46:10 -0800

    feat(posts): create new modal button

--- public/js/common.js
+++ public/js/common.js
@@ -310,7 +310,10 @@ function createPostHtml(postData, largeFont = false) {
 
     var buttons = "";
     if (postData.postedBy._id == userLoggedIn._id) {
-        buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal">
+        buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#confirmPinModal">
+        <i class="fas fa-thumbtack"></i>
+    </button>
+                    <button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal">
             <i class="fas fa-times"></i>
         </button>`
     }
