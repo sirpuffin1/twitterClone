7624f819e970bb09dbb71fce8cab63e47b784616
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-22 14:42:44 -0800

    feat(search): display posts

--- public/js/search.js
+++ public/js/search.js
@@ -22,6 +22,10 @@ function search(searchTerm, searchType) {
     var url = searchType == "users" ? "/api/users" : "/api/posts";
 
     $.get(url, {search: searchTerm }, (results) => {
-        console.log(results);
+        if(searchType == "users") {
+
+        } else {
+            displayPosts(results, $(".resultsContainer"))
+        }
     })
 }
