df6e54802ae7ce5c5fd7be6e63b3e0c26ae70e61
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-25 13:02:28 -0800

    feat(notifs): output notifications

--- public/js/notificationsPage.js
+++ public/js/notificationsPage.js
@@ -1,5 +1,79 @@
 $(document).ready(() => {
     $.get("/api/notifications", (data) => {
-        console.log(data)
+        outputNotificationList(data, $(".resultsContainer"))
     })
 })
+
+function outputNotificationList(notifications, container) {
+    notifications.forEach(notification => {
+        var html = createNotificationHtml(notification)
+        container.append(html)
+    })
+
+    if(notifications.length == 0) {
+        container.append("<span class='noResults'>Nothing to show.</span>")
+    }
+}
+
+function createNotificationHtml(notification) {
+    var userFrom = notification.userFrom;
+    var text = getNotificationText(notification)
+    var href = getNotificationUrl(notification);
+
+    return `<a href='${href}' class='resultListItem notification'>
+                <div class='resultsImageContainer'>
+                    <img src='${userFrom.profilePic}' alt='User's profile pic'>
+                </div>
+
+                <div class='resultsDetailsContainer ellipsis'>
+                    <span class='ellipsis'>
+                        ${text}
+                    </span>
+                </div>
+            </a>`
+}
+
+function getNotificationText(notification) {
+    var userFrom = notification.userFrom
+    if(!userFrom.firstName || !userFrom.lastName) {
+        return alert("User from data not populated")
+    }
+
+    var userFromName = `${userFrom.firstName} ${userFrom.lastName}`
+
+    var text;
+
+    if(notification.notificationType == "retweet") {
+        text = `${userFromName} retweeted one of your posts.`
+    }
+
+    else if(notification.notificationType == "like") {
+        text = `${userFromName} liked one of your posts.`
+    }
+
+    else if(notification.notificationType == "reply") {
+        text = `${userFromName} replied to one of your posts.`
+    }
+
+    else if(notification.notificationType == "follow") {
+        text = `${userFromName} followed you.`
+    }
+
+    return `<span class='ellipsis'>${text}</span>`
+}
+
+function getNotificationUrl(notification) {
+    var url = '#'
+
+    if(notification.notificationType == "retweet" || 
+    notification.notificationType == "like" ||
+    notification.notificationType == "reply") {
+        url = `/posts/${notification.entityId}`
+    }
+
+    else if(notification.notificationType == "follow") {
+        url = `/profile/${notification.entityId}`
+    }
+
+    return url
+}
