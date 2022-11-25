70e92ffec93379c1536e822ebc18cd468074a5d6
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-25 09:40:47 -0800

    feat(notifications): create static function on schema

--- schemas/NotificationSchema.js
+++ schemas/NotificationSchema.js
@@ -12,5 +12,16 @@ const NotificationSchema = new Schema({
   { timestamps: true }
 );
 
+NotificationSchema.statics.insertNotification = async (userTo, userFrom, notificationType, entityId) => {
+    var data = {
+        userTo: userTo,
+        userFrom: userFrom,
+        notificationType: notificationType,
+        entityId: entityId
+    };
+    await Notification.deleteOne(data).catch(error => console.log(error));
+    return Notification.create(data).catch(error => console.log(error));
+}
+
 var Notification = mongoose.model("Notification", NotificationSchema);
 module.exports = Notification;
