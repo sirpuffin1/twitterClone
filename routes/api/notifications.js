2fa226575d834b61a42e74e387b7ca0afc4d9262
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-25 12:24:42 -0800

    feat(app): create/import notif api route

---
+++ routes/api/notifications.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require('body-parser');
+const User = require('../../schemas/UserSchema');
+const Post = require('../../schemas/PostSchema')
+const Chat = require('../../schemas/ChatSchema');
+const Message = require('../../schemas/MessageSchema');
+const Notification = require('../../schemas/NotificationSchema');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+router.get("/", async(req, res, next) => {
+    res.status(200).send("it worked")
+})
+
+
+
+
+module.exports = router;
+
