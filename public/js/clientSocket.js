8bef46cb3a9bc3daecdba22e5282a789c06cb706
sirpuffin1 <angelcruz981@yahoo.com>
2022-11-24 17:24:47 -0800

    feat(app): emit an event to connect a user

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,3 +1,6 @@
 var connected = false;
 
-const socket = io("http://localhost:3003")
+const socket = io("http://localhost:3003");
+
+socket.emit("setup", userLoggedIn);
+socket.on("connected", () => connected = true);
